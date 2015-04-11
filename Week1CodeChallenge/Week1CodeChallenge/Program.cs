8b14eeea9be139431b8ef16fd318da100f23e179
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-04-11 13:37:49 -0600

    Initial Commit- FizzBuzz

--- Week1CodeChallenge/Week1CodeChallenge/Program.cs
+++ Week1CodeChallenge/Week1CodeChallenge/Program.cs
@@ -10,10 +10,37 @@ namespace Week1CodeChallenge
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+
+            for (int i = 92; i > 79; i--)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+            Console.ReadKey();
         }
+
+
         public static string FizzBuzz(int number)
         {
-            return string.Empty;
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number%5 == 0)
+            {
+                return "Fizz";
+            }
+            if (number%3 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return number.ToString();
+            }
         }
         public static string Yodaizer(string text)
         {

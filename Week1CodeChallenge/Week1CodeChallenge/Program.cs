84ff6d7db7f9205cee4102b7a0254084023167dc
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-04-12 16:36:25 -0600

    TextStats Function Complete

--- Week1CodeChallenge/Week1CodeChallenge/Program.cs
+++ Week1CodeChallenge/Week1CodeChallenge/Program.cs
@@ -10,7 +10,7 @@ namespace Week1CodeChallenge
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 21; i++)
             {
                 Console.WriteLine(FizzBuzz(i));
             }
@@ -21,6 +21,8 @@ namespace Week1CodeChallenge
             }
 
             Console.WriteLine(Yodaizer("I like code"));
+
+            TextStats("West philadelphia born and raised, on the playground is where I spent most of my days?");
 
             Console.ReadKey();
         }
@@ -45,19 +47,55 @@ namespace Week1CodeChallenge
                 return number.ToString();
             }
         }
-        public static string Yodaizer(string yoda)
+        public static string Yodaizer(string text)
         {
-            string[] theForce = yoda.Split(' ');
+            string[] theForce = text.Split(' ');
             Array.Reverse(theForce);
             return string.Join(" ", theForce);
         }
+
         public static bool IsPrime(int number)
         {
+            int primeCount = 0;
+
             return true;
         }
         public static string DashInsert(int number)
         {
             return string.Empty;
         }
+
+        public static void TextStats(string input)
+        {
+            int charCount = input.Length;
+            int vowelCount = 0;
+            int consoCount = 0;
+            int specCount = 0;
+            
+            string[] wordArray = input.Split(' ');
+            int wordCount = wordArray.Length;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].ToString() == "a" || input[i].ToString() == "e" || input[i].ToString() == "o" || input[i].ToString() == "i" || input[i].ToString() == "u" )
+                {
+                    vowelCount++;
+                }
+                else if (input[i].ToString() == "." || input[i].ToString() == " " || input[i].ToString() == "?")
+                {
+                    specCount++;
+                }
+                else
+                {
+                    consoCount++;
+                }
+            }
+            Console.WriteLine("Number of characters: " + charCount);
+            Console.WriteLine("Number of words: " + wordCount);
+            Console.WriteLine("Number of vowels: "+ vowelCount);
+            Console.WriteLine("Number of consonants: "+ consoCount);
+            Console.WriteLine("Number of special characters: "+ specCount);
+        }
+
     }
 }

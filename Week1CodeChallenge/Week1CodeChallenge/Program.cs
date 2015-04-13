959c0785167d732ab8aa9c67acf48245d3d9e31a
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-04-13 13:07:29 -0600

    complete

--- Week1CodeChallenge/Week1CodeChallenge/Program.cs
+++ Week1CodeChallenge/Week1CodeChallenge/Program.cs
@@ -24,10 +24,28 @@ namespace Week1CodeChallenge
 
             TextStats("West philadelphia born and raised, on the playground is where I spent most of my days?");
 
+            for (int i = 0; i <= 25; i++)
+            {
+                if (i %2 !=0)
+                {
+                    if (IsPrime(i) == true)
+                    {
+                        Console.WriteLine("{0} is a prime number", i);
+                    }
+                }
+            }
+
+            Console.WriteLine(DashInsert(454793));
+            Console.WriteLine(DashInsert(8675309));
+
             Console.ReadKey();
         }
 
-
+        /// <summary>
+        /// Based on what numbers are divisible by determines what will display. %5 = Fizz, %3= buzz, %3 and %5 = FizzBuzz
+        /// </summary>
+        /// <param name="number">number which you are entering to see its divisibility</param>
+        /// <returns></returns>
         public static string FizzBuzz(int number)
         {
             if (number % 5 == 0 && number % 3 == 0)
@@ -47,8 +65,15 @@ namespace Week1CodeChallenge
                 return number.ToString();
             }
         }
+
+        /// <summary>
+        /// Reverses the order of the words in a string
+        /// </summary>
+        /// <param name="text">string you wish to have words reversed</param>
+        /// <returns></returns>
         public static string Yodaizer(string text)
         {
+            // taking a string and turning it into an array, splitting up the string by spaces to determine where one word ends and the other starts
             string[] theForce = text.Split(' ');
             Array.Reverse(theForce);
             return string.Join(" ", theForce);
@@ -56,15 +81,42 @@ namespace Week1CodeChallenge
 
         public static bool IsPrime(int number)
         {
-            int primeCount = 0;
-
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
             return true;
         }
         public static string DashInsert(int number)
         {
-            return string.Empty;
+            //Convert number to string
+            string firstOne = number.ToString();
+
+            string second = string.Empty;
+
+            
+            foreach (char c in firstOne)
+            {
+                //Check to see if there is 2 odd numbers in a row
+                if (Convert.ToInt32(c) % 2 != 0 && Convert.ToInt32(second[second.Length - 1]) % 2 != 0)
+                {
+                    second = second + "-" + c;
+                }
+                else
+                {
+                    second = second + c;
+                }
+            }
+            return second.ToString();
         }
 
+        /// <summary>
+        /// Takes any string you enter and gives out stats about word count, character count, vowel count, spec char count, consonant count
+        /// </summary>
+        /// <param name="input">the string you wish to receive stats about</param>
         public static void TextStats(string input)
         {
             int charCount = input.Length;
@@ -75,13 +127,15 @@ namespace Week1CodeChallenge
             string[] wordArray = input.Split(' ');
             int wordCount = wordArray.Length;
 
+            // going through the string letter by letter and counting up the stats
             for (int i = 0; i < input.Length; i++)
             {
                 if (input[i].ToString() == "a" || input[i].ToString() == "e" || input[i].ToString() == "o" || input[i].ToString() == "i" || input[i].ToString() == "u" )
                 {
                     vowelCount++;
                 }
-                else if (input[i].ToString() == "." || input[i].ToString() == " " || input[i].ToString() == "?")
+                    // if it contains any of these add to special character count
+                else if (input[i].ToString() == "." || input[i].ToString() == " " || input[i].ToString() == "?" || input[i].ToString() == ",")
                 {
                     specCount++;
                 }
